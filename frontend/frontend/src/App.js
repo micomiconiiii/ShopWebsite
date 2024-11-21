8bdb0333d36dd4147c228be8596f7fedb0a36ecf
micomiconiiii <jhondelmico.abas@tup.edu.ph>
2024-11-21 14:46:55 +0800

    fifth commit

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
+import Shoes from "./pages/Shoes";
+import Add from "./pages/Add";
+import Update from "./pages/Update";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+        <Routes>
+          <Route path="/shoes" element={<Shoes/>}></Route>
+          <Route path="/add" element={<Add/>}></Route>          
+          <Route path="/update" element={<Update/>}></Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
